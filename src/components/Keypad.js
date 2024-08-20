8380eb8d6b5a79dae439e1027ecf89e35ced8bce
shannicec <shanice.cheptoo@student.moringaschool.com>
2024-08-20 17:08:27 +0300

    event handling lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,17 @@
-// Code Keypad Component Here
+import React from 'react';
 
-function Keypad (){
-    return (
-        <div></div>
-    )
-}
+const Keypad = () => {
+  const handleChange = () => {
+    console.log('Entering password...');
+  };
+
+  return (
+    <input 
+      type="password" 
+      onChange={handleChange} 
+      placeholder="Enter password"
+    />
+  );
+};
 
 export default Keypad;
